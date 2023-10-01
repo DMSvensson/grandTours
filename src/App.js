632cc1a365df1ea9958814ad55addae0d5ce7955
Daniel Møller Svensson <202007248@post.au.dk>
2023-10-01 20:20:52 +0200

    Added right scroll behavior

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    
+    this.state = {boxWidth: 0, currentStage: 0, square: undefined, scrollContainer: undefined};
+    this.viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+    this.scrollSpeed = 40;
+    
+    this.scrollBehavior = this.scrollBehavior.bind(this);
+  }
+  
+  render() {
+    return (
+      <div>
+        <div id="box" className='box' style={{width: `${this.state.boxWidth}px`}}></div>
+        <div onWheel={this.scrollBehavior} className='container mandatory-scroll-snapping'>
+          <div style={{height:'90vh', width:'100%', backgroundColor: 'red', position:'relative', zIndex:2}}>
+  
+          </div>
+          <div style={{height:'90vh', width:'100%', backgroundColor: 'blue', position:'relative', zIndex:2}}>
+  
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+  componentDidMount() {
+    this.setState({square: document.querySelector('#box')})
+    this.setState({scrollContainer: document.querySelector('.container')})
+  }
+  
+  scrollBehavior(event) {
+    if(event.deltaY < 0) {
+      // scroll up
+      if(this.state.boxWidth - this.scrollSpeed < 0) {
+        this.setState({boxWidth: 0});
+      } else {
+        this.setState({boxWidth: this.state.boxWidth - this.scrollSpeed});
+      }
+
+      if(this.state.square.getBoundingClientRect().width <= (this.state.currentStage * this.viewportWidth) + 80) {
+        this.setState({currentStage: this.state.currentStage - 1}, () => {
+          this.scrollTo();
+        });
+      }
+    } else {
+      // scroll down
+      if(this.state.boxWidth + this.scrollSpeed > this.viewportWidth * this.state.scrollContainer.children.length) {
+        this.setState({boxWidth: this.viewportWidth * this.state.scrollContainer.children.length});
+      } else {
+        this.setState({boxWidth: this.state.boxWidth + this.scrollSpeed});
+      }
+
+      if(this.state.square.getBoundingClientRect().width > ((this.state.currentStage + 1) * this.viewportWidth) + 20) {
+        this.setState({currentStage: this.state.currentStage + 1}, () => {
+          this.scrollTo();
+        });
+      }
+    }
+  }
+
+  scrollTo() {
+    window.scrollTo({
+      left: this.state.currentStage * this.viewportWidth,
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+  
 }
 
 export default App;
