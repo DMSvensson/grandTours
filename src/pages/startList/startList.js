b1792a7331e921bb82089c0e0c4855f3c0421dd2
Daniel Møller Svensson <202007248@post.au.dk>
2023-10-22 20:29:36 +0200

    Chnaged some style on startList

--- src/pages/startList/startList.js
+++ src/pages/startList/startList.js
@@ -26,10 +26,12 @@ function StartListPage() {
             <img className={styles.logo} src={logo} alt='TDF Logo' />
             <div className={styles.teamsContainer}>
                 <div className={styles.info}>
-                    <h1>Experience Tour de France 2022 by scolling</h1>
-                    <p>Scroll through all 21 stages of Tour de France 2022 to get a recap of every stage. For the best experience please use the website on a desktop or laptop</p>
-                    <p>To stay updated on all the exciting moments, route details, and results of the Tour de France
-                        please visit the official website of the Tour de France: <a href="https://www.letour.fr/en/" target="blank">Tour de France Official Website</a></p>
+                    <h1>Experience Tour de France 2022 by Scrolling</h1>
+                    <div>
+                        <p>Scroll through all 21 stages of Tour de France 2022 to get a recap of every stage. For the best experience please use the website on a desktop or laptop</p>
+                        <p>To stay updated on all the exciting moments, route details, and results of the Tour de France
+                            please visit the official website of the Tour de France: <a href="https://www.letour.fr/en/" target="blank">Tour de France Official Website</a></p>
+                    </div>
                 </div>
                 <Link to={`stages`} className={`btn btn-large ${styles.center}`}>Go to stages</Link>
                 <h2>Teams & Riders</h2>
